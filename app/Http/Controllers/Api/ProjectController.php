14ce38ba9db61262484f377cba19d57fe992245a
pannellalino <lista>
2023-01-30 16:35:27 +0100

    correzione dettaglio

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -8,8 +8,17 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
+    // restituisce tutti i progetti
     public function index(){
         $projects = Project::all();
         return response()->json(compact('projects'));
+    }
+
+    // restituisce un singolo progetto in base allo slug
+    public function show($slug)
+    {
+        $project = Project::where('slug', $slug)->first();
+
+        return response()->json($project);
     }
 }
